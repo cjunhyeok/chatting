885959ab55f0e74fa7b1221b95c3fad5a5c741cd
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-27 17:08:12 +0900

    Create Simple ChatRepository

---
+++ src/main/java/com/chat/chatting/repository/ChatRepository.java
@@ -0,0 +1,7 @@
+package com.chat.chatting.repository;
+
+import com.chat.chatting.domain.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+}
