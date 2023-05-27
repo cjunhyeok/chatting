08953496e82443fd24e664f9dddd45ab5a2ed8f8
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-27 19:08:50 +0900

    Delete ChatParticipation Entity with Repository

--- src/main/java/com/chat/chatting/repository/ChatParticipationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chat.chatting.repository;
-
-import com.chat.chatting.domain.ChatParticipation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatParticipationRepository extends JpaRepository<ChatParticipation, Long> {
-}
