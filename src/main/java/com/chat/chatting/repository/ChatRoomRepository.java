9dd938911d2ec1ed9c310a5d9c0526cb44a52de8
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-28 16:17:16 +0900

    Delete ChatRoom Entity with Repository

--- src/main/java/com/chat/chatting/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chat.chatting.repository;
-
-import com.chat.chatting.domain.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
