a556a1939b117579c5461f572a0ead5880c24330
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-28 16:19:20 +0900

    Create MemberService

---
+++ src/main/java/com/chat/chatting/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.chat.chatting.service;
+
+import com.chat.chatting.domain.Member;
+import com.chat.chatting.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(String username, String password, String nickname) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .build();
+
+        Member savedMember = memberRepository.save(member);
+
+        return savedMember.getId();
+    }
+
+    public Member findById(Long id) {
+        return memberRepository.findById(id).orElseThrow(()
+                -> new IllegalArgumentException("Member not exist"));
+    }
+}
