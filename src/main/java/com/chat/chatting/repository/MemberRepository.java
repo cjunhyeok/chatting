3b58931bbec2d419b8e9188bc7632af4f6453746
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-29 23:14:35 +0900

    Add findByUsername, countByUsername, findAll in MemberRepository

--- src/main/java/com/chat/chatting/repository/MemberRepository.java
+++ src/main/java/com/chat/chatting/repository/MemberRepository.java
@@ -3,5 +3,12 @@ package com.chat.chatting.repository;
 import com.chat.chatting.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByUsername(String username);
+    int countByUsername(String username);
+
+    List<Member> findAll();
 }
