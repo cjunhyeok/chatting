16011237bbbb8234a90b3dcb726c0c7159a565bf
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-05-30 18:06:16 +0900

    Create JwtAuthenticationFilter with jwt

---
+++ src/main/java/com/chat/chatting/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.chat.chatting.security.filter;
+
+import com.chat.chatting.api.dtos.LoginDto;
+import com.chat.chatting.security.signature.SecuritySigner;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWK;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final SecuritySigner securitySigner;
+    private final JWK jwk;
+
+    public JwtAuthenticationFilter(SecuritySigner securitySigner, JWK jwk) {
+        super(new AntPathRequestMatcher("/api/login"));
+        this.securitySigner = securitySigner;
+        this.jwk = jwk;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginDto loginDto = null;
+
+        try {
+            loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        return getAuthenticationManager().authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        User user = (User) authResult.getPrincipal();
+        String jwtToken;
+
+        try {
+            jwtToken = securitySigner.getJwtToken(user, jwk);
+            log.info("on success");
+            response.addHeader("Authorization", "Bearer " + jwtToken);
+        } catch (JOSEException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
